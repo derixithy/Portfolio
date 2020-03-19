649145074c44cca34d6a0d5eab9f5ab358e54aa1
Derixithy <derixithy@users.noreply.github.com>
2020-03-19 18:07:57 +0100

    Update index.blade.php
    list pages

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -3,14 +3,24 @@
 @section('page-title', $title)
 
 @if($pages)
-@section('content')
-	<main class="grid">
-
-		<ul>
-		@foreach($pages as $page)
-			<li>{{$page->title}}</li>
-		@endforeach
-		</ul>
-	</main>
-@endsection
+	@section('content')
+		<div class="grid">
+			@foreach($pages as $page)
+				<div class="column-9-12 mobile-column-4-6">{{$page->title}}</div>
+				<div class="column-2-12 mobile-column-1-6">{{$page->status}}</div>
+				<div class="column-1-12 mobile-column-1-6">
+					<a class="muted" href="{{route('page.edit', [$page->id])}}">Wijzig</a>
+				</div>
+			@endforeach
+		</div>
+	@endsection
+	@if(count($deleted) < 0)
+		@section('aside')
+			<ul class="menu">
+				<li class="title">Verwijderd</li>
+				@foreach($deleted as $page)
+					<li>$page->title</li>
+				@endforeach
+		@endsection
+	@endif
 @endif
