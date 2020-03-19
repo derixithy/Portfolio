471fb20b0f7e5792e091222d33cb35071c075859
Derixithy <derixithy@users.noreply.github.com>
2020-03-19 18:07:38 +0100

    Update admin.blade.php
    Added support for aside

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -28,7 +28,7 @@
 	</nav>
 	<ul class="subnav">
 		@foreach( $modules as $name => $title)
-		<li><a href="{{ url('admin/'.$name) }}">{{ $title }}</a></li>
+		<li><a href="{{ route($name.'.index') }}">{{ $title }}</a></li>
 		@endforeach
 	</ul>
 	<div class="container">
@@ -37,9 +37,20 @@
 		</div>
 @include('shared.errors')
 @section('main')
+	@hasSection('aside')
+		<div class="grid">
+			<main class="column-8-12">
+				@yield('content')
+			</main>
+			<aside class="column-4-12">
+				@yield('aside')
+			</aside>
+		</div>
+	@else
 		<main>
 			@yield('content')
 		</main>
+	@endif
 @show
 	</div>
 	<footer>
