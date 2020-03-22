3fd2888504f0dab196417a7c7e1bbbff92869858
Derixithy <derixithy@users.noreply.github.com>
2020-03-22 18:34:07 +0100

    added translations

---
+++ resources/lang/nl/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	// auth/login.blade.php
+	"email" => "E-Mail adres",
+	"enter-email" => "Typ je E-Mail hier",
+
+	"password" => "Wachtwoord",
+	"enter-password" => "Typ je wachtwoord hier",
+
+	"remember" => "Onthoud mij",
+	'forgot' => "Wachtwoord vergeten?",
+
+	'login' => "Inloggen",
+];
