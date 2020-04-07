c8f846dda5e44e2a8a04d467ef363f4bcf3621e8
Derixithy <derixithy@users.noreply.github.com>
2020-04-07 13:45:05 +0200

    Update index.blade.php
    Swap status view

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -8,8 +8,8 @@
         <thead>
             <tr>
                 <th class="max-width">Title</th>
-                <th>Status</th>
                 <th>Ouder</th>
+                <th>Status</th>
                 <th></th>
                 <th><a class="color-bg muted" href="{{route('project.create')}}"><span class="icon icon-sheet"></span></a></th>
             </tr>
@@ -17,8 +17,8 @@
 			@foreach($pages as $page)
 			<tr>
 				<td>{{$page->title}}</td>
-				<td>{{__('project.'.$page->status)}}</td>
 				<td><a href="{{route('page', $page->parent)}}">{{$page->parent}}</a></td>
+				<td>{{__('project.'.$page->status)}}</td>
 				<td><a class="muted" href="{{route('project.edit', [$page->id])}}">Wijzig</a></td>
 				<td><a class="muted" href="{{route('project.show', [$page->id])}}">Bekijk</a></td>
 			</tr>
