d73cf37afa5cfca60f96d78383911956aadf8578
Eddie Gjaltema <eddie@gjaltema-online.nl>
2020-09-14 10:47:58 +0200

    Update show.blade.php
    Update css grid, post tags and cover image

--- resources/views/page/show.blade.php
+++ resources/views/page/show.blade.php
@@ -3,6 +3,12 @@
 @section('page-title', $page->title)
 @section('cover', 'cover/'.basename($page->cover))
 
+@section('css')
+@parent
+.project img { width: 100%; max-height: 100%; }
+.tags { list-style-type: none; }
+@endsection
+
 @section('content')
 	<article>
 		<div class="text">
@@ -11,21 +17,23 @@
 	</article>
 @if($projects)
 	<div class="margin-bottom-huge"></div>
-	<div class="grid">
+	<div grid>
 		@foreach($page->projects as $project)
-		<div class="@if( $loop->first ) column-6-12 @else column-3-12 @endif mobile-column-3-6">
+		<div class="project" @if( $loop->first ) column="six" @else column="three" @endif>
 			<a href="{{ route('project', [$slug, $project->name]) }}" class="muted">
 				<div class="post">
 					<div class="card hover">
 						<div class="poster">
-							<img src="{{ $project->image }}" />
+							<img src="{{$project->cover ? '/cover/'.basename($project->cover) : 'img/cover.jpg'}}" style="width:100%; max-height:100%"/>
 						</div>
 						<div class="title">
 							<span>{{ $project->title }}</span>
 						</div>
 					</div>
 					<ul class="tags">
-						<li><a href="#">php</a></li><li><a href="#">javascript</a></li><li><a href="#">html</a></li><li><a href="#">css</a></li>
+						@foreach( $project->tags as $tag)
+						<li><a href="{{$tag->url}}">{{$tag->title}}</a></li>
+						@endforeach
 					</ul>
 				</div>
 			</a>
